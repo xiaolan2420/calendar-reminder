c7a36fb9025205cd28014a9746a426c6e8f91b99
皛懒 <73284314+xiaolan2420@users.noreply.github.com>
2024-09-26 19:52:15 +0800

    Update reminder.sql

--- sql/reminder.sql
+++ sql/reminder.sql
@@ -5,7 +5,7 @@ CREATE TABLE reminders
     content         TEXT        NOT NULL,
     reminder_at     DATETIME    NOT NULL,
     reminder_method VARCHAR(10) NOT NULL,
-    created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,                             -- 创建时间
-    updated_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- 更新时间
+    created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,                            
+    updated_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, 
     FOREIGN KEY (creator_id) REFERENCES users (id) ON DELETE CASCADE
 );
